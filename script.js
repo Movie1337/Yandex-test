a24e2f3a10038dc522e8543d6bc2e8ecd9f35673
Movie1337 <oldmooovie@gmail.com>
2024-10-11 22:32:12 +0300

    added transfer of items to cart when touched (fixed)

--- script.js
+++ script.js
@@ -5,49 +5,21 @@ let cartItems = 0;
 const maxItemsInRow = 3;
 const overlapPercentage = 50;
 
+const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+
 items.forEach((item) => {
-  item.addEventListener("dragstart", dragStart);
-  item.addEventListener("touchstart", touchStart);
+  if (!isMobile) {
+    item.addEventListener("dragstart", dragStart);
+  } else {
+    item.addEventListener("click", handleClick);
+  }
 });
 
 cart.addEventListener("dragover", dragOver);
 cart.addEventListener("drop", dropItem);
-cart.addEventListener("touchmove", touchMove);
-cart.addEventListener("touchend", touchEnd);
 
 function dragStart(event) {
   event.dataTransfer.setData("item-id", event.target.id);
-}
-
-function touchStart(event) {
-  const itemId = event.currentTarget.id;
-  const originalItem = document.getElementById(itemId);
-  originalItem.style.visibility = "hidden";
-
-  const touch = event.touches[0];
-  const newItem = document.createElement("img");
-  newItem.src = originalItem.src;
-  newItem.classList.add("item-in-cart");
-  cart.appendChild(newItem);
-  newItem.style.position = "absolute";
-  newItem.style.left = `${touch.clientX}px`;
-  newItem.style.top = `${touch.clientY}px`;
-
-  // Перемещение при касании
-  newItem.addEventListener("touchmove", (e) => {
-    const touchMove = e.touches[0];
-    newItem.style.left = `${touchMove.clientX}px`;
-    newItem.style.top = `${touchMove.clientY}px`;
-  });
-
-  newItem.addEventListener("touchend", () => {
-    positionItemInCart(newItem);
-    cartItems++;
-
-    if (cartItems >= 3) {
-      checkoutBtn.style.display = "block";
-    }
-  });
 }
 
 function dragOver(event) {
@@ -63,13 +35,22 @@ function dropItem(event) {
     originalItem.style.visibility = "hidden";
   }
 
+  addItemToCart(originalItem.src);
+}
+
+function handleClick(event) {
+  const originalItem = event.target;
+  addItemToCart(originalItem.src);
+  originalItem.style.visibility = "hidden";
+}
+
+function addItemToCart(itemSrc) {
   const newItem = document.createElement("img");
-  newItem.src = originalItem.src;
+  newItem.src = itemSrc;
   newItem.classList.add("item-in-cart");
   cart.appendChild(newItem);
 
   positionItemInCart(newItem);
-
   cartItems++;
 
   if (cartItems >= 3) {
@@ -87,7 +68,7 @@ function positionItemInCart(item) {
     item.style.left = `${leftPosition}px`;
     item.style.bottom = "20%";
   } else {
-    const randomLeft = Math.random() * 50;
+    const randomLeft = Math.random() * (100 - itemWidth);
     item.style.position = "absolute";
     item.style.left = `${randomLeft}%`;
     item.style.bottom = "20%";
